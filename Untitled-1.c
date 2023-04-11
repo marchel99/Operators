af15908d11087de9b598af6de182de7a68d5d382
marchel99 <kubamarchelski@live.com>
2023-04-11 21:45:32 +0200

    Changes to be committed: 	modified:   Untitled-1.c 	new file:   Untitled-1.exe 	new file:   tempCodeRunnerFile.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
-int main(
+
+int dodaj(int a, int b)
+{
+return a+b;
+}
+
+int odejmij(int a, int b)
+{
+return a-b;
+}
+
+
+int main(){
     
 
 
 
 
+typedef int (*pf)();
+
+ pf functions[2] = {dodaj,odejmij};
+
+ printf("%d",functions[0](2,3) );
+
     return 0;
-)
+}
