6adf103cb7439cd8993e6b23ef61115cbe68c7cb
marchel99 <kubamarchelski@live.com>
2023-04-11 22:02:23 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   modulo.c

---
+++ modulo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int dodaj(int a, int b)
+{
+return a+b;
+}
+
+int odejmij(int a, int b)
+{
+return a-b;
+}
+
+int main(){
+typedef int (*pf)();
+ pf functions[2] = {dodaj,odejmij};
+
+/* 
+//int (*functons[2])()= {dodaj,odejmij};
+int (*tablica[5])(int, int)={dodaj, odejmij};
+int wynik=functions[0](2,3);
+wynik==5 ? printf("OK\n\n") :   printf("ZLE!\n\n");
+ */
+
+
+// 1%5  2%5 3%5 4%5 5%5
+// 6%5  7%5 8%5  9%5  10%5
+
+7%5==2 ? printf("OK\n\n"):printf("NIE!\n\n") ;
+
+
+    return 0;
+}
