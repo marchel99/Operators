b90452431049325e6390b4b27049f74087bb1bf6
marchel99 <kubamarchelski@live.com>
2023-04-12 23:40:44 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   bitwise-operators.c

--- bitwise-operators.c
+++ bitwise-operators.c
@@ -2,63 +2,60 @@
 
 void clear()
 {
-    printf("\n");
+     printf("\n");
 }
 
-void printBinary(int num) {
-    for (int i = 8; i >= 0; i--) {
-        int bit = (num >> i) & 1;
-        printf("%d", bit);
-    }
-        clear();
+void printBinary(int num)
+{
+     for (int i = 8; i >= 0; i--)
+     {
+          int bit = (num >> i) & 1;
+          printf("%d", bit);
+     }
+     clear();
 }
 
-int main() {
-    
-   unsigned int a = 0b0010;
-   unsigned int b = 0b1000;
+int main()
+{
 
-   // Bitwise AND operator
-   unsigned int c =  a&b ;
-   printf("a & b = %d\n", c);
-    printBinary(c);  
-        clear();
+     unsigned int a = 0b0010;
+     unsigned int b = 0b1000;
 
-   // Bitwise OR operator
-   unsigned int d =  a|b ;
-   printf("a | b = %d\n", d);
-   printBinary(d);  
-        clear();
+     // Bitwise AND operator
+     unsigned int c = a & b;
+     printf("a & b = %d\n", c);
+     printBinary(c);
+     clear();
 
-   // Bitwise XOR operator
-   unsigned int e = a ^ b;
-   printf("a ^ b = %d\n", e);
-     printBinary(e);  
-        clear();
+     // Bitwise OR operator
+     unsigned int d = a | b;
+     printf("a | b = %d\n", d);
+     printBinary(d);
+     clear();
 
+     // Bitwise XOR operator
+     unsigned int e = a ^ b;
+     printf("a ^ b = %d\n", e);
+     printBinary(e);
+     clear();
 
-   // Bitwise NOT operator
-   unsigned int f = ~a;
-   printf("~a = %d\n", f);
-     printBinary(f);  
-        clear();
+     // Bitwise NOT operator
+     unsigned int f = ~a;
+     printf("~a = %d\n", f);
+     printBinary(f);
+     clear();
 
-   // Left shift operator
-   unsigned int g = a << 2;
-   printf("a << 2 = %d\n", g);
-   printBinary(g);  
-        clear();
+     // Left shift operator
+     unsigned int g = a << 2;
+     printf("a << 2 = %d\n", g);
+     printBinary(g);
+     clear();
 
+     // Right shift operator
+     unsigned int h = b >> 2;
+     printf("b >> 2 = %d\n", h);
+     printBinary(h);
+     clear();
 
-
-   // Right shift operator
-   unsigned int h = b >> 2;
-   printf("b >> 2 = %d\n", h);
- printBinary(h);  
-        clear();
-
-
-
-
-   return 0;
+     return 0;
 }
