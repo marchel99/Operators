cb4e3b444cde2827d07475ac5783aae84919b570
marchel99 <kubamarchelski@live.com>
2023-04-12 23:37:30 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   bitwise-operators.c
    	modified:   bitwise-operators.exe

--- bitwise-operators.c
+++ bitwise-operators.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 
+void clear()
+{
+    printf("\n");
+}
+
+void printBinary(int num) {
+    for (int i = 8; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        printf("%d", bit);
+    }
+        clear();
+}
+
 int main() {
+    
    unsigned int a = 0b0010;
    unsigned int b = 0b1000;
                        
 
-
-
    // Bitwise AND operator
    unsigned int c =  a&b ;
    printf("a & b = %d\n", c);
+    printBinary(c);  
+        clear();
 
    // Bitwise OR operator
    unsigned int d =  a|b ;
    printf("a | b = %d\n", d);
-
+   printBinary(d);  
+        clear();
 
    // Bitwise XOR operator
    unsigned int e = a ^ b;
    printf("a ^ b = %d\n", e);
+     printBinary(e);  
+        clear();
 
 
    // Bitwise NOT operator
    unsigned int f = ~a;
    printf("~a = %d\n", f);
+     printBinary(f);  
+        clear();
 
    // Left shift operator
    unsigned int g = a << 2;
