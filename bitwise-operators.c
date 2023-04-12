55a7d626c3bdc45e1681dd4922c6a4b38b7c19a7
marchel99 <kubamarchelski@live.com>
2023-04-12 23:29:54 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   bitwise-operators.c
    	modified:   bitwise-operators.exe
    	modified:   tempCodeRunnerFile.c

--- bitwise-operators.c
+++ bitwise-operators.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 
 int main() {
-   unsigned char a = 0x0F;
-   unsigned char b = 0x0F;
+   unsigned int a = 0010;
+   unsigned int b = 1000;
+                       1
 
 
 
-
-   unsigned char c = a & b;
+   // Bitwise AND operator
+   unsigned int c =  a&b ;
    printf("a & b = %d\n", c);
 
-
-
    // Bitwise OR operator
-   unsigned char d = a | b;
+   unsigned int d =  a|b ;
    printf("a | b = %d\n", d);
 
+
    // Bitwise XOR operator
-   unsigned char e = a ^ b;
+   unsigned int e = a ^ b;
    printf("a ^ b = %d\n", e);
 
+
    // Bitwise NOT operator
-   unsigned char f = ~a;
+   unsigned int f = ~a;
    printf("~a = %d\n", f);
 
    // Left shift operator
-   unsigned char g = a << 2;
+   unsigned int g = a << 2;
    printf("a << 2 = %d\n", g);
 
    // Right shift operator
-   unsigned char h = b >> 2;
+   unsigned int h = b >> 2;
    printf("b >> 2 = %d\n", h);
 
    return 0;
